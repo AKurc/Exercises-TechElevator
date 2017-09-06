ce99d7b3f475d98458427d46a49c0ec2c929ce1e
Anna Kurchak <AKurchak@gmail.com>
2017-09-06 08:21:53 -0400

    fibonacci

--- m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
+++ m1-w1d5-command-line-programs-exercises/src/main/java/com/techelevator/Fibonacci.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 /*
  The Fibonacci numbers are the integers in the following sequence:  
 	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
@@ -17,7 +19,25 @@ Please enter the Fibonacci number: 25
 public class Fibonacci {
 
 	public static void main(String[] args) {
-
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter the Fiboncci number: ");
+		
+		int limit = input.nextInt();
+		input.nextLine();
+		
+		fibonacciSeq(limit);
 	}
 
+	public static void fibonacciSeq(int limit) {
+		int previous = 0;
+		System.out.print("0");
+		
+		for (int next = 1; next < limit; ) {
+			System.out.print("," + next);
+			int temp = previous + next;
+			previous = next;
+			next = temp;
+		}
+	}
 }
